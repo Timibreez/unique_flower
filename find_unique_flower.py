ad64d0fcba36f8ebb79f35cfe1e71aecb18852d3
Timibreez <ffftimi@gmail.com>
2022-07-11 15:13:12 +0100

    initial commit

---
+++ find_unique_flower.py
@@ -0,0 +1,18 @@
+
+def create_flowerdict(filename):
+    flower_dict = {}
+    with open(filename) as flowers:
+        for flower in flowers:
+            key, val = flower.lower().strip().split(': ')
+            flower_dict[key.strip()] = val.strip()
+    return flower_dict
+
+
+def main():
+    flower_d = create_flowerdict('flowers.txt')
+    fullname = input('Enter your First [space] Last name only: ')
+    first_name = fullname[0].lower()
+    first_letter = first_name[0]
+    print('Unique flower name with the first letter: {}'.format(flower_d[first_letter]))
+
+main()
